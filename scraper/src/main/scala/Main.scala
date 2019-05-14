e840eb71a95efb25ac3d3274a4cb233a4ed12c9c
Arkadiusz Kraus <arkadius.kraus@gmail.com>
2019-05-14 19:45:02 +0200

    checking for stops at the beginning

--- scraper/src/main/scala/Main.scala
+++ scraper/src/main/scala/Main.scala
@@ -10,15 +10,24 @@ object Main extends App {
   val system = ActorSystem("scraperSystem")
   val scheduler = system.actorOf(Scheduler.props, "scheduler")
   implicit val executor = system.dispatcher
-  //scheduler ! RecurringTask("stops", new StopsRequest, 10 seconds)
-  //scheduler ! Task(new StopsRequest)
-  //scheduler ! Task(new VehiclesRequest)
+  Database.stopRepository().findAll()
+      .andThen{ case Success(stops) =>
+        if(stops.isEmpty) {
+          println("No stops. Downloading new")
+          scheduler ! Task(new StopsRequest)
+          scheduler ! Task(new VehiclesRequest)
+        } else {
+          println("Stops already exist")
+        }
+      }
+  println("Waiting for stops")
+  Thread.sleep(30000)
   Database.stopRepository().findAll()
       .andThen { case Success(stops) =>
         println("There are " + stops.size + "stops")
         stops.foreach(stop => {
-          scheduler ! Task(new StopRequest(stop.shortName))
-          Thread.sleep(1000)
+          scheduler ! RecurringTask(s"stop${stop.shortName}", new StopRequest(stop.shortName), 1 minutes)
+          Thread.sleep(500)
         })
       }
 }
